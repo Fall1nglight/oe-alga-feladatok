28d34edee33a64830421b28d43eb1820e233befc
Fall1ngLight <keruloszabolcs1@gmail.com>
2025-10-30 11:02:09 +0100

    🔧 fix: namespaces

--- ALGA/Optimalizalas/07_NyersEro.cs
+++ ALGA/Optimalizalas/07_NyersEro.cs
@@ -1,156 +1,155 @@
 ﻿using System;
 using OE.ALGA.Adatszerkezetek;
 
-namespace OE.ALGA.Optimalizalas
+namespace OE.ALGA.Optimalizalas;
+
+// 7. heti labor feladat - Tesztek: 07_NyersEroTesztek.cs
+public class HatizsakProblema
 {
-    // 7. heti labor feladat - Tesztek: 07_NyersEroTesztek.cs
-    public class HatizsakProblema
+    // fields & properties
+
+    /// <summary>
+    /// Tárgyak száma
+    /// </summary>
+    public int n { get; }
+
+    /// <summary>
+    /// Hátizsák mérete
+    /// </summary>
+    public int Wmax { get; }
+
+    /// <summary>
+    /// Egyes tárgyak súlyai
+    /// </summary>
+    public int[] w { get; }
+
+    /// <summary>
+    /// Egyes tárgyak értékei
+    /// </summary>
+    public float[] p { get; }
+
+    // constructors
+    public HatizsakProblema(int n, int Wmax, int[] w, float[] p)
     {
-        // fields & properties
-
-        /// <summary>
-        /// Lehetséges megoldások száma
-        /// </summary>
-        public int n { get; }
-
-        /// <summary>
-        /// Hátizsák mérete
-        /// </summary>
-        public int Wmax { get; }
-
-        /// <summary>
-        /// Egyes tárgyak súlyai
-        /// </summary>
-        public int[] w { get; }
-
-        /// <summary>
-        /// Egyes tárgyak értékei
-        /// </summary>
-        public float[] p { get; }
-
-        // constructors
-        public HatizsakProblema(int n, int Wmax, int[] w, float[] p)
-        {
-            this.n = n;
-            this.Wmax = Wmax;
-            this.w = w;
-            this.p = p;
-        }
-
-        // methods
-        public int OsszSuly(bool[] pakolas)
-        {
-            int sum = 0;
-
-            for (int i = 0; i < n; i++)
-            {
-                sum += pakolas[i] ? w[i] : 0;
-            }
-
-            return sum;
-        }
-
-        public float OsszErtek(bool[] pakolas)
-        {
-            float sum = 0;
-
-            for (int i = 0; i < n; i++)
-            {
-                sum += pakolas[i] ? p[i] : 0;
-            }
-
-            return sum;
-        }
-
-        public bool Ervenyes(bool[] pakolas) => OsszSuly(pakolas) <= Wmax;
+        this.n = n;
+        this.Wmax = Wmax;
+        this.w = w;
+        this.p = p;
     }
 
-    public class NyersEro<T>
+    // methods
+    public int OsszSuly(bool[] pakolas)
     {
-        // fields & properties
-        private int m; // lehetséges megoldások száma
-        private Func<int, T> generator;
-        private Func<T, float> josag;
-        public int LepesSzam { get; private set; } // összehasonlítások száma
+        int sum = 0;
 
-        // constructors
-        public NyersEro(int m, Func<int, T> generator, Func<T, float> josag)
+        for (int i = 0; i < n; i++)
         {
-            this.m = m;
-            this.generator = generator;
-            this.josag = josag;
+            sum += pakolas[i] ? w[i] : 0;
         }
 
-        // methods
-        public T OptimalisMegoldas()
-        {
-            T opt = generator(1);
-
-            for (int i = 2; i <= m; i++)
-            {
-                T atmenetiOpt = generator(i);
-
-                if (josag(atmenetiOpt) > josag(opt))
-                    opt = atmenetiOpt;
-
-                LepesSzam++;
-            }
-
-            return opt;
-        }
+        return sum;
     }
 
-    public class NyersEroHatizsakPakolas
+    public float OsszErtek(bool[] pakolas)
     {
-        // fields & properties
-        public int LepesSzam { get; private set; }
-        private HatizsakProblema problema;
+        float sum = 0;
 
-        // constructors
-        public NyersEroHatizsakPakolas(HatizsakProblema problema)
+        for (int i = 0; i < n; i++)
         {
-            this.problema = problema;
+            sum += pakolas[i] ? p[i] : 0;
         }
 
-        // methods
-        public bool[] Generator(int x)
-        {
-            Lista<bool> lista = new LancoltLista<bool>();
-
-            int szam = x - 1;
-
-            for (int i = 0; i < problema.n; i++)
-            {
-                lista.Hozzafuz((szam / (int)Math.Pow(2, i)) % 2 == 1);
-            }
-
-            bool[] result = new bool[lista.Elemszam];
-
-            for (int i = 0; i < lista.Elemszam; i++)
-                result[i] = lista.Kiolvas(i);
-
-            return result;
-        }
-
-        public float Josag(bool[] pakolas) =>
-            problema.Ervenyes(pakolas) ? problema.OsszErtek(pakolas) : -1;
-
-        public bool[] OptimalisMegoldas()
-        {
-            int lehetsegesMegoldasokSzama = (int)Math.Pow(2, problema.n);
-
-            NyersEro<bool[]> nyersEro = new NyersEro<bool[]>(
-                lehetsegesMegoldasokSzama,
-                Generator,
-                Josag
-            );
-
-            bool[] megoldas = nyersEro.OptimalisMegoldas();
-            LepesSzam = nyersEro.LepesSzam;
-
-            return megoldas;
-        }
-
-        public float OptimalisErtek() => problema.OsszErtek(OptimalisMegoldas());
+        return sum;
     }
+
+    public bool Ervenyes(bool[] pakolas) => OsszSuly(pakolas) <= Wmax;
+}
+
+public class NyersEro<T>
+{
+    // fields & properties
+    private int m; // lehetséges megoldások száma
+    private Func<int, T> generator;
+    private Func<T, float> josag;
+    public int LepesSzam { get; private set; } // összehasonlítások száma
+
+    // constructors
+    public NyersEro(int m, Func<int, T> generator, Func<T, float> josag)
+    {
+        this.m = m;
+        this.generator = generator;
+        this.josag = josag;
+    }
+
+    // methods
+    public T OptimalisMegoldas()
+    {
+        T opt = generator(1);
+
+        for (int i = 2; i <= m; i++)
+        {
+            T atmenetiOpt = generator(i);
+
+            if (josag(atmenetiOpt) > josag(opt))
+                opt = atmenetiOpt;
+
+            LepesSzam++;
+        }
+
+        return opt;
+    }
+}
+
+public class NyersEroHatizsakPakolas
+{
+    // fields & properties
+    public int LepesSzam { get; private set; }
+    private HatizsakProblema problema;
+
+    // constructors
+    public NyersEroHatizsakPakolas(HatizsakProblema problema)
+    {
+        this.problema = problema;
+    }
+
+    // methods
+    public bool[] Generator(int x)
+    {
+        LancoltLista<bool> lista = new LancoltLista<bool>();
+
+        int szam = x - 1;
+
+        for (int i = 0; i < problema.n; i++)
+        {
+            lista.Hozzafuz((szam / (int)Math.Pow(2, i)) % 2 == 1);
+        }
+
+        bool[] result = new bool[lista.Elemszam];
+
+        for (int i = 0; i < lista.Elemszam; i++)
+            result[i] = lista.Kiolvas(i);
+
+        return result;
+    }
+
+    public float Josag(bool[] pakolas) =>
+        problema.Ervenyes(pakolas) ? problema.OsszErtek(pakolas) : -1;
+
+    public bool[] OptimalisMegoldas()
+    {
+        int lehetsegesMegoldasokSzama = (int)Math.Pow(2, problema.n);
+
+        NyersEro<bool[]> nyersEro = new NyersEro<bool[]>(
+            lehetsegesMegoldasokSzama,
+            Generator,
+            Josag
+        );
+
+        bool[] megoldas = nyersEro.OptimalisMegoldas();
+        LepesSzam = nyersEro.LepesSzam;
+
+        return megoldas;
+    }
+
+    public float OptimalisErtek() => problema.OsszErtek(OptimalisMegoldas());
 }
--- ALGA/Optimalizalas/08_DinamikusProgramozas.cs
+++ ALGA/Optimalizalas/08_DinamikusProgramozas.cs
@@ -1,105 +1,104 @@
 ﻿using System;
 
-namespace OE.ALGA.Optimalizalas
+namespace OE.ALGA.Optimalizalas;
+
+// 8. heti labor feladat - Tesztek: 08_DinamikusProgramozasTesztek.cs
+
+public class DinamikusHatizsakPakolas
 {
-    // 8. heti labor feladat - Tesztek: 08_DinamikusProgramozasTesztek.cs
+    private HatizsakProblema problema;
+    public int LepesSzam { get; private set; }
 
-    public class DinamikusHatizsakPakolas
+    public DinamikusHatizsakPakolas(HatizsakProblema problema)
     {
-        private HatizsakProblema problema;
-        public int LepesSzam { get; private set; }
+        this.problema = problema;
+    }
 
-        public DinamikusHatizsakPakolas(HatizsakProblema problema)
+    private float[,] TablazatFeltoltes()
+    {
+        // +1 szükséges mert egy plusz sor és oszlop is lesz
+        float[,] F = new float[problema.w.Length + 1, problema.Wmax + 1];
+
+        int rowCount = F.GetLength(0);
+        int colCount = F.GetLength(1);
+
+        // először fel kell tölteni az első oszlopot
+        for (int i = 0; i < rowCount; i++)
         {
-            this.problema = problema;
+            F[i, 0] = 0;
         }
 
-        private float[,] TablazatFeltoltes()
+        // majd feltöltjük az első sort
+        for (int i = 0; i < colCount; i++)
         {
-            // +1 szükséges mert egy plusz sor és oszlop is lesz
-            float[,] F = new float[problema.w.Length + 1, problema.Wmax + 1];
+            F[0, i] = 0;
+        }
 
-            int rowCount = F.GetLength(0);
-            int colCount = F.GetLength(1);
-
-            // először fel kell tölteni az első oszlopot
-            for (int i = 0; i < rowCount; i++)
+        // bejárjuk a 2D-s tömböt, az első oszlopot+sort kihagyva
+        // t -> tárgyak száma
+        // h -> szabad hely (súly) száma
+        for (int t = 1; t < rowCount; t++)
+        {
+            for (int h = 1; h < colCount; h++)
             {
-                F[i, 0] = 0;
-            }
+                LepesSzam++;
+                // ha h > 0 és t > 0 és h < w[t]
+                // ha van szabad hely
+                // és van tárgy amit a hátizssákba szeretnénk pakolni
+                // az előző két feltétel fölösleges, mert a ciklus (t=1 és h=1)-től indul
 
-            // majd feltöltjük az első sort
-            for (int i = 0; i < colCount; i++)
-            {
-                F[0, i] = 0;
-            }
-
-            // bejárjuk a 2D-s tömböt, az első oszlopot+sort kihagyva
-            // t -> tárgyak száma
-            // h -> szabad hely (súly) száma
-            for (int t = 1; t < rowCount; t++)
-            {
-                for (int h = 1; h < colCount; h++)
+                // ha az adott (i.) tárgy nem fér bele a hátizsákba (h < w[t])
+                // w[t-1] => indexelés miatt
+                if (h < problema.w[t - 1])
                 {
-                    LepesSzam++;
-                    // ha h > 0 és t > 0 és h < w[t]
-                    // ha van szabad hely
-                    // és van tárgy amit a hátizssákba szeretnénk pakolni
-                    // az előző két feltétel fölösleges, mert a ciklus (t=1 és h=1)-től indul
+                    F[t, h] = F[t - 1, h];
+                }
+                // ha az adott (i.) tárgy belefér a hátizsákba (h >= w[t])
+                else
+                {
+                    float result = Math.Max(
+                        F[t - 1, h],
+                        F[t - 1, h - problema.w[t - 1]] + problema.p[t - 1]
+                    );
 
-                    // ha az adott (i.) tárgy nem fér bele a hátizsákba (h < w[t])
-                    // w[t-1] => indexelés miatt
-                    if (h < problema.w[t - 1])
-                    {
-                        F[t, h] = F[t - 1, h];
-                    }
-                    // ha az adott (i.) tárgy belefér a hátizsákba (h >= w[t])
-                    else
-                    {
-                        float result = Math.Max(
-                            F[t - 1, h],
-                            F[t - 1, h - problema.w[t - 1]] + problema.p[t - 1]
-                        );
-
-                        F[t, h] = result;
-                    }
+                    F[t, h] = result;
                 }
             }
-
-            return F;
         }
 
-        public float OptimalisErtek()
+        return F;
+    }
+
+    public float OptimalisErtek()
+    {
+        // mivel +1-e hozzáadtunk
+        // ezek az értékek megadják az utolsó elemet
+        return TablazatFeltoltes()[problema.w.Length, problema.Wmax];
+    }
+
+    public bool[] OptimalisMegoldas()
+    {
+        float[,] F = TablazatFeltoltes();
+        // annyi elemű tömbnek kell lennie, ahány tárgy van
+        // =>
+        // mind egyes index egy tárgyat reprezentál
+        int t = problema.w.Length;
+        int h = problema.Wmax;
+
+        bool[] O = new bool[t];
+
+        while (t > 0 && h > 0)
         {
-            // mivel +1-e hozzáadtunk
-            // ezek az értékek megadják az utolsó elemet
-            return TablazatFeltoltes()[problema.w.Length, problema.Wmax];
-        }
-
-        public bool[] OptimalisMegoldas()
-        {
-            float[,] F = TablazatFeltoltes();
-            // annyi elemű tömbnek kell lennie, ahány tárgy van
-            // =>
-            // mind egyes index egy tárgyat reprezentál
-            int t = problema.w.Length;
-            int h = problema.Wmax;
-
-            bool[] O = new bool[t];
-
-            while (t > 0 && h > 0)
+            // ReSharper disable once CompareOfFloatsByEqualityOperator
+            if (F[t, h] != F[t - 1, h])
             {
-                // ReSharper disable once CompareOfFloatsByEqualityOperator
-                if (F[t, h] != F[t - 1, h])
-                {
-                    O[t - 1] = true;
-                    h -= problema.w[t - 1];
-                }
-
-                t -= 1;
+                O[t - 1] = true;
+                h -= problema.w[t - 1];
             }
 
-            return O;
+            t -= 1;
         }
+
+        return O;
     }
 }
