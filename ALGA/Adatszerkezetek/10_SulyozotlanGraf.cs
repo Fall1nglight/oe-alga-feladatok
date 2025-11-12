84c8384b638efddd6c05d5a3f44b74a5924fc71f
Fall1ngLight <keruloszabolcs1@gmail.com>
2025-11-12 12:18:22 +0100

    ✅ finish: week-09, week-10

--- ALGA/Adatszerkezetek/10_SulyozotlanGraf.cs
+++ ALGA/Adatszerkezetek/10_SulyozotlanGraf.cs
@@ -3,4 +3,233 @@
 namespace OE.ALGA.Adatszerkezetek
 {
     // 10. heti labor feladat - Tesztek: 10_SulyozatlanGrafTesztek.cs
+
+    /// <summary>
+    /// Egész szám csúcsokat tartalmazó súlyozatlan gráf éleit reprezentálja
+    /// </summary>
+    public class EgeszGrafEl : GrafEl<int>, IComparable
+    {
+        /// <summary>
+        /// Melyik csúcsból mutat az él
+        /// </summary>
+        public int Honnan { get; }
+
+        /// <summary>
+        /// Melyik csúcsba mutat az él
+        /// </summary>
+        public int Hova { get; }
+
+        public EgeszGrafEl(int honnan, int hova)
+        {
+            Honnan = honnan;
+            Hova = hova;
+        }
+
+        public int CompareTo(object? other)
+        {
+            if (ReferenceEquals(this, other))
+                return 0;
+
+            if (ReferenceEquals(null, other))
+                throw new NullReferenceException();
+
+            if (other is not EgeszGrafEl b)
+                throw new ArgumentException();
+
+            if (Honnan != b.Honnan)
+                return Honnan.CompareTo(b.Honnan);
+
+            return Hova.CompareTo(b.Hova);
+        }
+    }
+
+    /// <summary>
+    /// Megvalósítja a súlyozott gráf interfész által előírt műveleteket
+    /// </summary>
+    public class CsucsmatrixSulyozatlanEgeszGraf : SulyozatlanGraf<int, EgeszGrafEl>
+    {
+        /// <summary>
+        /// Csúcsok száma
+        /// </summary>
+        private int n;
+
+        /// <summary>
+        /// Csúcsmátrix | M[i, j] => i. csúcsból vezet-e él j. csúcsba
+        /// </summary>
+        private bool[,] M;
+
+        /// <summary>
+        /// Megadja a csúcsok azonosítóit (0->n-1)
+        /// </summary>
+        public Halmaz<int> Csucsok => GetCsucsok();
+
+        /// <summary>
+        /// Megadja a gráfban található éleket
+        /// </summary>
+        public Halmaz<EgeszGrafEl> Elek => GetElek();
+
+        /// <summary>
+        /// Megadja a gráfban található csúcsok számát
+        /// </summary>
+        public int CsucsokSzama => n;
+
+        /// <summary>
+        /// Megadja a gráfban található élek számát
+        /// </summary>
+        public int ElekSzama => GetElekSzama();
+
+        public CsucsmatrixSulyozatlanEgeszGraf(int n)
+        {
+            this.n = n;
+            M = new bool[n, n];
+        }
+
+        public bool VezetEl(int honnan, int hova)
+        {
+            return M[honnan, hova];
+        }
+
+        public Halmaz<int> Szomszedai(int csucs)
+        {
+            FaHalmaz<int> szomszedok = new FaHalmaz<int>();
+
+            for (int j = 0; j < M.GetLength(1); j++)
+            {
+                if (M[csucs, j])
+                    szomszedok.Beszur(j);
+            }
+
+            return szomszedok;
+        }
+
+        public void UjEl(int honnan, int hova)
+        {
+            M[honnan, hova] = true;
+        }
+
+        private int GetElekSzama()
+        {
+            // kell egy változó, amiben eltároljuk az összeget (sum)
+            // bejárjuk a mátrix-ot
+            // ha az adott elem értéke 'igaz' növeljük a sum értékét 1-el
+            // bejárás után visszaadjuk a sum értékét
+
+            int sum = 0;
+
+            for (int i = 0; i < M.GetLength(0); i++)
+            {
+                for (int j = 0; j < M.GetLength(1); j++)
+                {
+                    if (M[i, j])
+                        sum++;
+                }
+            }
+
+            return sum;
+        }
+
+        private FaHalmaz<int> GetCsucsok()
+        {
+            FaHalmaz<int> csucsok = new FaHalmaz<int>();
+
+            for (int i = 0; i < n; i++)
+                csucsok.Beszur(i);
+
+            return csucsok;
+        }
+
+        private FaHalmaz<EgeszGrafEl> GetElek()
+        {
+            FaHalmaz<EgeszGrafEl> elek = new FaHalmaz<EgeszGrafEl>();
+
+            for (int i = 0; i < M.GetLength(0); i++)
+            {
+                for (int j = 0; j < M.GetLength(1); j++)
+                {
+                    if (!M[i, j])
+                        continue;
+
+                    elek.Beszur(new EgeszGrafEl(i, j));
+                }
+            }
+
+            return elek;
+        }
+    }
+
+    /// <summary>
+    /// Megvalósítja a szélességi és mélységi bejárást
+    /// </summary>
+    public class GrafBejarasok
+    {
+        /// <summary>
+        /// Végrehatja az adott gráf-on a szélességi bejárás algoritmust
+        /// </summary>
+        /// <param name="g">Bejárandó gráf</param>
+        /// <param name="start">Bejárás kezdőpontja (csúcs)</param>
+        /// <param name="eljaras">Végrehajtandó művelet az elemeken</param>
+        /// <typeparam name="V">Csúcsok típusa</typeparam>
+        /// <typeparam name="E">Élek típusa</typeparam>
+        /// <returns></returns>
+        public static Halmaz<V> SzelessegiBejaras<V, E>(Graf<V, E> g, V start, Action<V> eljaras)
+            where V : IComparable
+        {
+            Sor<V> sor = new TombSor<V>(g.CsucsokSzama);
+            Halmaz<V> elertElemek = new FaHalmaz<V>();
+
+            sor.Sorba(start);
+            elertElemek.Beszur(start);
+
+            while (!sor.Ures)
+            {
+                V csucs = sor.Sorbol();
+
+                // feldolgozzuk a csúcsot
+                eljaras(csucs);
+
+                // bejárjuka a csúcs szomszédait
+                g.Szomszedai(csucs)
+                    .Bejar(szomszed =>
+                    {
+                        // ha még nem értük el az adott elemet
+                        if (!elertElemek.Eleme(szomszed))
+                        {
+                            sor.Sorba(szomszed);
+                            elertElemek.Beszur(szomszed);
+                        }
+                    });
+            }
+
+            return elertElemek;
+        }
+
+        public static Halmaz<V> MelysegiBejaras<V, E>(Graf<V, E> g, V start, Action<V> eljaras)
+            where V : IComparable
+        {
+            Halmaz<V> elertElemek = new FaHalmaz<V>();
+
+            MelysegiBejarasRekurzio(g, start, elertElemek, eljaras);
+
+            return elertElemek;
+        }
+
+        public static void MelysegiBejarasRekurzio<V, E>(
+            Graf<V, E> g,
+            V jelenlegiCsucs,
+            Halmaz<V> elertElemek,
+            Action<V> eljaras
+        )
+            where V : IComparable
+        {
+            elertElemek.Beszur(jelenlegiCsucs);
+            eljaras(jelenlegiCsucs);
+
+            g.Szomszedai(jelenlegiCsucs)
+                .Bejar(szomszed =>
+                {
+                    if (!elertElemek.Eleme(szomszed))
+                        MelysegiBejarasRekurzio<V, E>(g, szomszed, elertElemek, eljaras);
+                });
+        }
+    }
 }
--- ALGA/Optimalizalas/09_VisszalepesesKereses.cs
+++ ALGA/Optimalizalas/09_VisszalepesesKereses.cs
@@ -3,4 +3,276 @@
 namespace OE.ALGA.Optimalizalas
 {
     // 9. heti labor feladat - Tesztek: 09VisszalepesesKeresesTesztek.cs
+
+    public class VisszalepesesOptimalizacio<T>
+    {
+        // fields & properties
+
+        /// <summary>
+        /// Részproblémák/szintek száma
+        /// </summary>
+        protected int n;
+
+        /// <summary>
+        /// Egyes szinteken a lehetséges megoldások száma
+        /// </summary>
+        protected int[] M;
+
+        /// <summary>
+        /// Lehetséges részmegoldásokat tárolja
+        /// M[x,i] lesz az x-edik szint i-edik megoldása
+        /// </summary>
+        protected T[,] R;
+
+        /// <summary>
+        /// Az algoritmus első korlát függvényét tárolja
+        /// Az ft(x,r) -> igaz, ha az x-edik szinten az r egy választható részmegoldás
+        /// </summary>
+        protected Func<int, T, bool> ft;
+
+        /// <summary>
+        /// Az algoritmus második korlát függvényét tárolja
+        /// Az fk(x,r,E) -> igaz, ha az x-edik szinten az r egy válstzható részmegoldás
+        /// akkor, ha az előző szinteken az E-ben lévő részmegoldásokat választottuk
+        /// </summary>
+        protected Func<int, T, T[], bool> fk;
+
+        /// <summary>
+        /// Az egyes teljes megoldások értékelését végzi
+        /// A josag(r) visszaadja az r teljes megoldás értéke
+        /// </summary>
+        protected Func<T[], float> josag;
+
+        public int LepesSzam { get; protected set; }
+
+        public VisszalepesesOptimalizacio(
+            int n,
+            int[] M,
+            T[,] R,
+            Func<int, T, bool> ft,
+            Func<int, T, T[], bool> fk,
+            Func<T[], float> josag
+        )
+        {
+            this.n = n;
+            this.M = M;
+            this.R = R;
+            this.ft = ft;
+            this.fk = fk;
+            this.josag = josag;
+        }
+
+        /// <summary>
+        /// Legjobb megoldást keressük visszalépéses kereséssel
+        /// </summary>
+        /// <param name="szint">Az aktuálisan vizsgált részprobléma száma</param>
+        /// <param name="E">Az eddig választott részmegoldásokat tárolja</param>
+        /// <param name="van">Eddig talált-e már teljest megoldást</param>
+        /// <param name="O">Az eddigi talált legjobb optimális eredmény</param>
+        protected virtual void Backtrack(int szint, ref T[] E, ref bool van, ref T[] O)
+        {
+            int i = 0;
+
+            // bejárjuk a lehetséges szinten lévő megoldásokat
+            while (i < M[szint])
+            {
+                LepesSzam++;
+                i++;
+
+                // Ha az adott szinten választható az r megoldás
+                if (ft(szint, R[szint, i - 1]))
+                {
+                    // Az adott szinten választható az r megoldás, ha eddig
+                    // az E részmegoldásokat választottuk
+                    if (fk(szint, R[szint, i - 1], E))
+                    {
+                        E[szint] = R[szint, i - 1];
+
+                        if (szint == n - 1)
+                        {
+                            if (!van || josag(E) > josag(O))
+                                O = (T[])E.Clone();
+
+                            van = true;
+                        }
+                        else
+                        {
+                            Backtrack(szint + 1, ref E, ref van, ref O);
+                        }
+                    }
+                }
+            }
+        }
+
+        public T[] OptimalisMegoldas()
+        {
+            T[] E = new T[n];
+            T[] O = new T[n];
+            bool van = false;
+
+            Backtrack(0, ref E, ref van, ref O);
+
+            return O;
+        }
+    }
+
+    public class VisszalepesesHatizsakPakolas
+    {
+        protected HatizsakProblema problema;
+        public int LepesSzam { get; protected set; }
+
+        public VisszalepesesHatizsakPakolas(HatizsakProblema problema)
+        {
+            this.problema = problema;
+        }
+
+        public virtual bool[] OptimalisMegoldas()
+        {
+            int[] M = new int[problema.n];
+
+            for (int i = 0; i < M.Length; i++)
+                M[i] = 2;
+
+            bool[,] R = new bool[problema.n, 2];
+
+            for (int i = 0; i < R.GetLength(0); i++)
+            {
+                R[i, 0] = true;
+                R[i, 1] = false;
+            }
+
+            VisszalepesesOptimalizacio<bool> visszalepesesOptimalizacio =
+                new VisszalepesesOptimalizacio<bool>(
+                    problema.n,
+                    M,
+                    R,
+                    (_, _) => true,
+                    (szint, r, e) =>
+                    {
+                        bool[] copy = (bool[])e.Clone();
+                        copy[szint] = r;
+                        return problema.Ervenyes(copy);
+                    },
+                    problema.OsszErtek
+                );
+
+            bool[] optMegoldas = visszalepesesOptimalizacio.OptimalisMegoldas();
+
+            LepesSzam = visszalepesesOptimalizacio.LepesSzam;
+
+            return optMegoldas;
+        }
+
+        public float OptimalisErtek()
+        {
+            return problema.OsszErtek(OptimalisMegoldas());
+        }
+    }
+
+    public class SzetvalasztasEsKorlatozasOptimalizacio<T> : VisszalepesesOptimalizacio<T>
+    {
+        protected Func<int, T[], float> fb;
+
+        public SzetvalasztasEsKorlatozasOptimalizacio(
+            int n,
+            int[] M,
+            T[,] R,
+            Func<int, T, bool> ft,
+            Func<int, T, T[], bool> fk,
+            Func<T[], float> josag,
+            Func<int, T[], float> fb
+        )
+            : base(n, M, R, ft, fk, josag)
+        {
+            this.fb = fb;
+        }
+
+        protected override void Backtrack(int szint, ref T[] E, ref bool van, ref T[] O)
+        {
+            int i = 0;
+
+            while (i < M[szint])
+            {
+                LepesSzam++;
+                i++;
+
+                if (ft(szint, R[szint, i - 1]))
+                {
+                    if (fk(szint, R[szint, i - 1], E))
+                    {
+                        E[szint] = R[szint, i - 1];
+
+                        if (szint == n - 1)
+                        {
+                            if (!van || josag(E) > josag(O))
+                                O = (T[])E.Clone();
+
+                            van = true;
+                        }
+                        else
+                        {
+                            if (josag(E) + fb(szint, E) > josag(O))
+                                Backtrack(szint + 1, ref E, ref van, ref O);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public class SzetvalasztasEsKorlatozasHatizsakPakolas : VisszalepesesHatizsakPakolas
+    {
+        public SzetvalasztasEsKorlatozasHatizsakPakolas(HatizsakProblema problema)
+            : base(problema) { }
+
+        public override bool[] OptimalisMegoldas()
+        {
+            int[] M = new int[problema.n];
+
+            for (int i = 0; i < M.Length; i++)
+                M[i] = 2;
+
+            bool[,] R = new bool[problema.n, 2];
+
+            for (int i = 0; i < R.GetLength(0); i++)
+            {
+                R[i, 0] = true;
+                R[i, 1] = false;
+            }
+
+            SzetvalasztasEsKorlatozasOptimalizacio<bool> opt =
+                new SzetvalasztasEsKorlatozasOptimalizacio<bool>(
+                    problema.n,
+                    M,
+                    R,
+                    (_, _) => true,
+                    (szint, r, e) =>
+                    {
+                        bool[] copy = (bool[])e.Clone();
+                        copy[szint] = r;
+                        return problema.Ervenyes(copy);
+                    },
+                    problema.OsszErtek,
+                    (szint, e) =>
+                    {
+                        float b = 0;
+
+                        for (int i = szint; i < problema.n; i++)
+                        {
+                            if (problema.OsszSuly(e) + problema.w[i] <= problema.Wmax)
+                            {
+                                b += problema.p[i];
+                            }
+                        }
+
+                        return b;
+                    }
+                );
+
+            bool[] optMegoldas = opt.OptimalisMegoldas();
+            LepesSzam = opt.LepesSzam;
+
+            return optMegoldas;
+        }
+    }
 }
